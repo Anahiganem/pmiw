c968dff3f5d30baa7b5597cebd9e3429aee2abb1
Anahiganem <168236893+Anahiganem@users.noreply.github.com>
2024-11-12 18:52:29 -0300

    Personaje.js

--- tpfinalparte2/Personaje.js
+++ tpfinalparte2/Personaje.js
@@ -1,14 +1,15 @@
 class Personaje {
-  constructor() {
-    this.posX = 300;
-    this.posY = 300;
+  constructor(posX, posY) {
+    this.posX = posX;
+    this.posY = posY;
     this.colorper = color(0, 190, 0);
     this.vidas = 3;
     //this.linternasRecolectadas = new linternasRecolectadas();
   }
 
   dibujar() {
-    fill(0);
+    noStroke();
+    fill(this.colorper);
     ellipse(this.posX, this.posY, 45, 45); //cabeza
     rect(this.posX-10, this.posY, 20, 50); // torso
     line(this.posX - 10, this.posY + 10, this.posX - 20, this.posY + 20); // Brazo izquierdo
@@ -16,11 +17,20 @@ class Personaje {
   }
 
   movderecha() {
+     this.posX += 10;
   }
 
   movizquierda() {
+    this.posX -= 10;
   }
-
+  
+  teclasMovimiento(keyCode) {
+    if (keyCode == LEFT_ARROW) {
+      this.movizquierda();
+    } else if (keyCode == RIGHT_ARROW) {
+      this.movderecha();
+    }
+  }
   estaVivo() {
   }
 
